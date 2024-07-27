c2bc6f1179b544eb14a759b9c5b3979492ed99d9
Razvan Paros <razvanparos21@gmail.com>
2024-07-27 19:02:25 +0300

    added images

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-scroll';
 function Header() {
   return (
     <div className="header-div padding" name='main'>
-        <h2 style={{fontWeight:800,fontSize:"32px", cursor:"pointer"}}>razvanparos.</h2>
+        <h2 style={{fontWeight:800,fontSize:"32px", cursor:"pointer"}}><Link activeClass='active' to='main' smooth={true} duration={500} offset={-100}>razvanparos.</Link></h2>
         <ul className='header-menu'>
             <li><Link activeClass='active' to='main' smooth={true} duration={500} offset={-100}>Home.</Link></li>
             <li><Link activeClass='active' to='skills' smooth={true} duration={500} offset={-30}>Skills.</Link></li>
--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -3,7 +3,13 @@ import './ProjectCard.css';
 function ProjectCard(props) {
   return (
     <div className="projects-card">
-        <p>{props.data.title}</p>
+      <div className='project-images'>
+        <img className='project-img' src={props.data.img1} alt="" />
+        {props.data.img2!=""?<img className='project-img' src={props.data.img2} alt="" />:''}
+        {props.data.img3!=""?<img className='project-img' src={props.data.img3} alt="" />:''}
+      </div>
+      
+      <p>{props.data.title}</p>
     </div>
   );
 }
