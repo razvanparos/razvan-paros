6e2803f61e6acb04ee1b27d09521ee8b67b80d8e
Razvan Paros <razvanparos21@gmail.com>
2024-07-29 01:20:14 +0300

    desktop complete

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,7 @@
 import './Contact.css';
+import { Link } from 'react-scroll';
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FiGithub } from "react-icons/fi";
 
 function Contact() {
     let currentDate = new Date();
@@ -7,12 +10,33 @@ function Contact() {
   return (
     <div className="contact-div padding" name='contact'>
        <div className='contact-content'>
-            <div className='contact-line'>
-                <div className='line'></div>
-                <p>razvanparos.</p>
-                <div className='line'></div>
+          <div className='contact-lists'>
+            <ul className='contact-menu'>
+              <li>Quick Links</li>
+              <li><Link activeClass='active' to='main' smooth={true} duration={500} offset={-100}>Home.</Link></li>
+              <li><Link activeClass='active' to='skills' smooth={true} duration={500} offset={-30}>Skills.</Link></li>
+              <li><Link activeClass='active' to='projects' smooth={true} duration={500} offset={30}>Projects.</Link></li>
+            </ul>
+            <ul className='contact-menu2'>
+              <li>Contact</li>
+              <li>razvanparos21@gmail.com</li>
+              <li>+40 721474061</li>
+            </ul>
+            <div className='header-menu'>
+              <a href='https://github.com/razvanparos' target='_blank' className='border'><FiGithub className='socials-icon'/></a>
+              <a href='https://www.linkedin.com/in/razvanparos-frontend/' target='_blank' className='border'><FaLinkedinIn className='socials-icon'/></a>
+              <a href='https://www.linkedin.com/in/razvanparos-frontend/overlay/1722204895839/single-media-viewer/?profileId=ACoAAD3ONOsB0zh_zefve425N3RrQJ9MUCxkXGo' target='_blank' className='border'><p className='socials-icon'>CV</p></a>
             </div>
-            <p>© Copyright {currentYear} | Razvan Paros | All rights reserved</p>
+          </div>
+          <div style={{width:'100%', display:'flex',flexDirection:'column', alignItems:'center', rowGap:'12px'}}>
+             <div className='contact-line'>
+              <div className='line'></div>
+              <p>razvanparos.</p>
+              <div className='line'></div>
+            </div>
+            <p>©Copyright {currentYear} | Răzvan Paroș | All rights reserved</p>
+          </div>
+         
        </div>
     </div>
   );
