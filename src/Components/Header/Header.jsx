ca922c3428f188813342cb0a0a6b769b0a394afd
Razvan Paros <razvanparos21@gmail.com>
2024-07-30 18:55:54 +0300

    added mobile dropdown

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-scroll';
 
 function Header() {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
   return (
     <div className="header-div padding" name='main'>
         <h2 style={{fontWeight:800,fontSize:"32px", cursor:"pointer"}}><Link activeClass='active' to='main' smooth={true} duration={500} offset={-100}>razvanparos.</Link></h2>
@@ -11,6 +14,20 @@ function Header() {
             <li><Link activeClass='active' to='projects' smooth={true} duration={500} offset={30}>Projects.</Link></li>
             <li><Link activeClass='active' to='contact' smooth={true} duration={500}>Contact.</Link></li>
         </ul>
+        <label class="burger" for="burger" >
+          <input type="checkbox" id="burger" checked={mobileMenu} onClick={()=>{setMobileMenu(!mobileMenu)}}/>
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+        <div className={`dropdown ${mobileMenu ? 'open':'close'}`}>
+          <ul className='header-menu-mobile'>
+              <li><Link activeClass='active' to='main' smooth={true} duration={500} offset={-100} onClick={()=>{setMobileMenu(!mobileMenu)}}>Home.</Link></li>
+              <li><Link activeClass='active' to='skills' smooth={true} duration={500} offset={-30} onClick={()=>{setMobileMenu(!mobileMenu)}}>Skills.</Link></li>
+              <li><Link activeClass='active' to='projects' smooth={true} duration={500} offset={30} onClick={()=>{setMobileMenu(!mobileMenu)}}>Projects.</Link></li>
+              <li><Link activeClass='active' to='contact' smooth={true} duration={500} onClick={()=>{setMobileMenu(!mobileMenu)}}>Contact.</Link></li>
+          </ul>
+        </div>
     </div>
   );
 }
